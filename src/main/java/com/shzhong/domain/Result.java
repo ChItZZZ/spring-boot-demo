52cc22d4b4d274aebf645cb76820bffe8bad5036
zhongshaohan <zhongshaohan@meituan.com>
2018-01-24 17:53:51 +0800

    add

--- src/main/java/com/shzhong/domain/Result.java
+++ src/main/java/com/shzhong/domain/Result.java
@@ -5,40 +5,21 @@ package com.shzhong.domain;
  */
 
 
+import lombok.Data;
+
 /**
  * http请求返回的最外层对象
  */
+
+@Data
 public class Result<T> {
-//    错误码
+    //    错误码
     private Integer code;
 
-//    提示信息
+    //    提示信息
     private String msg;
 
-//    具体内容
-    private  T data;
+    //    具体内容
+    private T data;
 
-    public Integer getCode() {
-        return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
 }
